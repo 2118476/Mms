fef8a2b1cc240aa305faf6620646e61aeac81b1c
2118476 <117785648+2118476@users.noreply.github.com>
2025-07-12 12:18:51 +0100

    Update callback URLs to Render

--- backend/sms/src/main/java/com/mms/sms/service/CallService.java
+++ backend/sms/src/main/java/com/mms/sms/service/CallService.java
@@ -3,6 +3,7 @@ package com.mms.sms.service;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Call;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
@@ -16,18 +17,21 @@ public class CallService {
     private final String token = System.getenv("TWILIO_AUTH_TOKEN");
     private final String from = System.getenv("TWILIO_PHONE_NUMBER");
 
-    private static final String BASE_CALLBACK_URL = "https://mms-backend-5erf.onrender.com/api/outbound-call";
+    @Value("${twilio.base.url}")
+    private String baseCallbackUrl;
 
     public String makeCall(String to) {
         Twilio.init(sid, token);
 
         try {
-            String callbackWithNumber = BASE_CALLBACK_URL + "?to=" + URLEncoder.encode(to, StandardCharsets.UTF_8.toString());
+            String callbackWithNumber = baseCallbackUrl + "/outbound-call?to=" +
+                    URLEncoder.encode(to, StandardCharsets.UTF_8.toString());
 
             Call call = Call.creator(
                     new PhoneNumber(to),
                     new PhoneNumber(from),
-                    new URI(callbackWithNumber)).create();
+                    new URI(callbackWithNumber)
+            ).create();
 
             return "📞 Call started! SID: " + call.getSid();
         } catch (Exception e) {
