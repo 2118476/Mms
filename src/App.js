2165d029e00dd15f24d075b40b75b3b8eeb900f2
2118476 <117785648+2118476@users.noreply.github.com>
2025-07-12 13:23:05 +0100

    Update callback URLs to Render

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 import React, { useState } from 'react';
 import SMSForm from './components/SMSForm';
 import CallForm from './components/CallForm';
 import { useTranslation } from 'react-i18next';
+import './App.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
